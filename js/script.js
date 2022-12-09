b2646d24d4c35013e174aa736ca5d9aaaac33ff7
Rusik-best86 <119488421+Rusik-best86@users.noreply.github.com>
2022-12-09 16:30:12 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -52,3 +52,19 @@ $(document).ready(function () {
       $(this).toggleClass('hidden').next().slideToggle(300);
    });
 });
+
+// кнопка наверх
+
+$('.back').click(function () {
+   $('body,html').animate({ scrollTop: 0 }, 800); // 800 - Скорость анимации
+});
+
+$(window).scroll(function () { // Отслеживаем начало прокрутки
+   let scrolled = $(window).scrollTop(); // Вычисляем сколько было прокручено.
+
+   if (scrolled > 350) { // Если высота прокрутки больше 350 - показываем кнопку
+      $('.back').addClass('active');
+   } else {
+      $('.back').removeClass('active');
+   }
+});
